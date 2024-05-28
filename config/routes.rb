5901fdcabc485faff5231f6cf1d3df240c7f1305
sanjan1000 <siddiquesanjan90@gmail.com>
2024-05-29 00:19:42 +0600

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root 'posts#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_151306) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_160549) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -30,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_151306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
