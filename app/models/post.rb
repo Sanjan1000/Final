c323ec70a0c3464ab2d635e7b47d69069fa61c86
sanjan1000 <siddiquesanjan90@gmail.com>
2024-05-30 21:46:27 +0600

    final1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :nested_posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  ransacker :title_or_body_cont, formatter: proc { |v| v } do |parent|
+    Arel::Nodes::Grouping.new(
+      parent.table[:title].matches("%#{parent.bind}%").or(parent.table[:body].matches("%#{parent.bind}%"))
+    )
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    super + ["title_or_body_cont"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
