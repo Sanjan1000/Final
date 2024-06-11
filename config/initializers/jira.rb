57b63713843769e7027e6db849aa986722ac9e7d
sanjan1000 <siddiquesanjan90@gmail.com>
2024-06-11 16:26:01 +0600

    Add config/initializers/jira.rb to .gitignore

--- config/initializers/jira.rb
+++
@@ -1,7 +0,0 @@
-JIRA_OPTIONS = {
-  site: 'https://mabsiddiquesanjan.atlassian.net/',
-  context_path: '',
-  auth_type: :basic,
-  username: 'm.a.b.siddique.sanjan@gmail.com',  # replace with your Jira email
-  password: 'ATATT3xFfGF02VvldRcLcAmXCa6RtfsfCQiYSnRSVgCGF07bkntrwPApfRw4HZqZ6vE2NRNm0PcGCaPblRZ1jxJu4RWQJNPPkr79YiHIdyVrMUWydXS6bkxzcVf8jrgAwFNwJZxxrp-MzG7jEH9QpmJDO8WV867QDl06cgkTEhINu8CxfaWbbOM=67AF04AB'           # replace with your Jira API token
-}
