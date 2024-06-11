5b47980cb5ebf4b297074c8cbb59d2644e8f1855
sanjan1000 <siddiquesanjan90@gmail.com>
2024-06-11 18:47:43 +0600

    api

---
+++ config/initializers/jira.rb
@@ -0,0 +1,7 @@
+JIRA_OPTIONS = {
+  site: 'https://mabsiddiquesanjan.atlassian.net/',
+  context_path: '',
+  auth_type: :basic,
+  username: 'm.a.b.siddique.sanjan@gmail.com',  # replace with your Jira email
+  password: 'ATATT3xFfGF0tpiugbuFg9ZgOjfqAU32a1Rxa2adXYHehf5pWjxdw33ziB8tRB34__DrM3af_SHtHCb7iN7gHQOgh6b5bASsokgpdYQbot5UtBQKif-F_9C7yWC0Q5tBmtbYR8C7wsSolEMEaGfzFd_a_lBc--5_e3y1shxPXKp6AZ4DrVgddm0=06B0934C'
+}
